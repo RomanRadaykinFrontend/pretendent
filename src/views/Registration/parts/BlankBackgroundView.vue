3c651df8e3b27ccf8989a139b57cb900320105af
Роман Радайкин <rradaikin@mobiledep.ru>
2021-07-15 14:44:39 +0300

    40 - поменял картинку бланка на странице регистрации

--- src/views/Registration/parts/BlankBackgroundView.vue
+++ src/views/Registration/parts/BlankBackgroundView.vue
@@ -17,6 +17,11 @@ export default class BlankBackgroundView extends Vue {}
   background-image: url("../../../common/images/blank.png")
   background-repeat: no-repeat
   background-position: center
+  background-size: auto
   width: 100%
   height: 100%
+
+@media screen and (max-width: 1500px)
+  .blank-background-view
+    background-size: 70%
 </style>
