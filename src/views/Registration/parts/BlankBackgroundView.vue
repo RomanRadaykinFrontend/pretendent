3f6c4b73f03e2fd12017491aa7da3e479c38ca53
Роман Радайкин <rradaikin@mobiledep.ru>
2021-07-19 10:57:40 +0300

    fix

--- src/views/Registration/parts/BlankBackgroundView.vue
+++ src/views/Registration/parts/BlankBackgroundView.vue
@@ -13,7 +13,7 @@ export default class BlankBackgroundView extends Vue {}
 <style scoped lang="sass">
 @import '../../../common/assets/common'
 .blank-background-view
-  background-color: rgb(250, 250, 251)
+  background-color: rgb(250, 250, 252)
   background-image: url("../../../common/images/blank.png")
   background-repeat: no-repeat
   background-position: center
