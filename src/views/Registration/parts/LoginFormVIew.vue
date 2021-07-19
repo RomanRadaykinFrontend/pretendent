3c411cb94e471ad99acbacd98d1f20bcfc612350
Роман Радайкин <rradaikin@mobiledep.ru>
2021-07-19 14:44:00 +0300

    fix

--- src/views/Registration/parts/LoginFormVIew.vue
+++ src/views/Registration/parts/LoginFormVIew.vue
@@ -71,7 +71,8 @@ export default class LoginFormView extends Vue {
 
   get isLoginDataCorrect(){
     const isCorrectEmail = this.user.email ? this.emailValidate.test( this.user.email ) : false
-    const isCorrectTelegram = this.user.telegram !== '' ? this.telegramValidate.test( this.user.telegram ) : true
+    const isCorrectTelegram = this.user.telegram && this.user.telegram !== '' ?
+      this.telegramValidate.test( this.user.telegram ) : true
     const isCorrectLastName = this.user.name ? this.nameValidate.test( this.user.lastName ) : false
     const isCorrectName = this.user.lastName ? this.nameValidate.test( this.user.name ) : false
 
