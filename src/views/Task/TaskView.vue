f997bf41416ed451aab8f3e8f1aea445085f4151
Роман Радайкин <rradaikin@mobiledep.ru>
2021-07-20 12:39:21 +0300

    add background image

--- src/views/Task/TaskView.vue
+++ src/views/Task/TaskView.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
   .task-view
+    .task-view__header
+      .task-view__question-title Вопрос {{ currentQuestion.id }}
+      .task-view__answers-title Варианты ответов
     TaskBody(
       :current-question="currentQuestion"
       :question-total-count="questionTotalCount"
@@ -43,7 +46,8 @@ export default class TaskView extends Vue {
 .task-view
   width: 100%
   height: 100%
-  background: url("./../../common/images/task-background.png")
+  background: url("./../../common/images/task-background.png") no-repeat
+  background-size: cover
   box-sizing: border-box
   padding: 20px
   position: relative
@@ -52,6 +56,17 @@ export default class TaskView extends Vue {
   flex-direction: column
   align-items: center
   font-family: Roboto, serif
+  &__header
+    display: flex
+    justify-content: space-between
+    width: 1100px
+  &__question-title, &__answers-title
+    width: 50%
+    padding: 10px 20px
+    font-family: Roboto, serif
+    font-style: normal
+    font-weight: normal
+    font-size: 20px
 
 @media screen and (max-width: 1024px)
   .task-view
